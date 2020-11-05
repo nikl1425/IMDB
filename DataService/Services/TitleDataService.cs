a154d21c9102d3bfc881fd0c23e03ff1b8d2879d
nikl1425 <niklas@astne.dk>
2020-11-05 11:08:13 +0100

    created TitleDataService.cs and TitleDataServiceTest.cs

---
+++ DataService/Services/TitleDataService.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace TitleDataService
+{
+    public class TitleDataService
+    {
+        //private readonly string _connectionString;
+        public TitleDataService()
+        {
+           //using var ctx = new ImdbContext();
+        }
+
+/*
+        public IList<> GetCategories()
+        {
+            using var ctx = new NorthWindContext();
+            return ctx.Categories.ToList();
+        }
+
+          */
+    }
+}
---
+++ UnitTest/TitleDataServiceTest.cs
@@ -0,0 +1,7 @@
+﻿namespace PortFolio2.Tests
+{
+    public class TitleDataServiceTest
+    {
+        
+    }
+}
