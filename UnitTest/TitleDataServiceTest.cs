c865c1054f10e9ae616fce28404b28652e7f355b
nikl1425 <niklas@astne.dk>
2020-11-05 11:16:30 +0100

    installeret xunit

--- UnitTest/TitleDataServiceTest.cs
+++ UnitTest/TitleDataServiceTest.cs
@@ -1,7 +1,17 @@
-﻿namespace PortFolio2.Tests
+﻿using System;
+using System.Linq;
+using Xunit;
+
+
+namespace PortFolio2.Tests
 {
     public class TitleDataServiceTest
     {
-        
+        public void GetOrders()
+        {
+            var service = new DataService();
+            var orders = service.GetOrders();
+            Assert.Equal(830, orders.Count);
+        }
     }
 }
