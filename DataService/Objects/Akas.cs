9c8c524a746e869bbe7833ddff80a9516ce4fa60
nikl1425 <niklas@astne.dk>
2020-11-05 13:02:02 +0100

    title akas title type akas type titleAKas

--- DataService/Objects/Akas.cs
+++ DataService/Objects/Akas.cs
@@ -2,6 +2,15 @@
 {
     public class Akas
     {
-        
+        public int Id { get; set; }
+        public string TitleId { get; set; }
+        public int Ordering { get; set; }
+        public string AkasName { get; set; }
+        public string Region { get; set; }
+        public string Language { get; set; }
+        public string Type { get; set; }
+        public bool IsOriginalTitle { get; set; }
+        public Akas_Akas_Type AkasAkasType { get; set; }
+        public Title Title { get; set; }
     }
 }
