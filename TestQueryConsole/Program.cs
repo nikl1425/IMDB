5b6c8cccccc98a1fd2bb89d58bf7086146ce6b91
nikl1425 <niklas@astne.dk>
2020-11-10 11:24:07 +0100

    Tilføjet console test project

---
+++ TestQueryConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using DataService;
+using DataService.Objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestQueryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title getTitlePersonName(string id)
+            {
+                using var ctx = new ImdbContext();
+                var query = ctx.title
+                    .Include(x => x.TitlePersons)
+                    .ThenInclude(x => x.Person)
+                    .FirstOrDefault(x => x.Id == id);
+                return query;
+            }
+
+            getTitlePersonName("tt0098286");
+        }
+    }
+}
