bc01fc3e3188b6548ee80b23fcd9a791c411e372
jaco4336 <jjfivel@gmail.com>
2020-11-10 16:44:46 +0100

    Added IPsonDataService

---
+++ DataService/Services/Interface/IPersonDataService.cs
@@ -0,0 +1,15 @@
+﻿using DataService.Objects;
+
+namespace DataService.Services
+{
+    public interface IPersonDataService
+    {
+        Person GetPerson(string id);
+        Person_known_title GetPersonKnownTitle(int id);
+        Person_Person_Known_Title GetPersonPersonKnownTitle(int id);
+        Person GetProfessionByPersonId(string id);
+        Profession GetPersonAmountByProfession (string profession);
+        Person_Profession GetPersonProfession(int id);
+        Profession GetProfession(int id);
+    }
+}
